6a543af493b108a53be8b7b3d3c08f1a949a8769
jayedalnahian <jnahian752@gmail.com>
2026-05-21 18:46:01 +0600

    better auth proxy added

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,21 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizeCss: true,
   },
+  // better-auth proxy
+  async rewrites() {
+    return [
+      {
+        // Explicitly map auth requests
+        source: "/api/auth/:path*",
+        destination: process.env.NEXT_PUBLIC_BACKEND_URL + "/api/auth/:path*",
+      },
+      {
+        // Explicitly map v1 API requests
+        source: "/api/v1/:path*",
+        destination: process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
